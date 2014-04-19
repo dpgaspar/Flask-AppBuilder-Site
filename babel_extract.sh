2fb4c3c05f679ef8045366550217fc7c919b2170
dpgaspar <danielvazgaspar@gmail.com>
2014-04-19 18:19:04 +0100

    translations

--- babel_extract.sh
+++ babel_extract.sh
@@ -1,5 +1,5 @@
-pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
+pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot app
 pybabel update -N -i ./babel/messages.pot -d app/translations
-pybabel compile -f -d app/translations
+pybabel compile -d app/translations
 
 
