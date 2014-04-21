414adf2b4c8768a99878461ef0e299c00ea27f90
dpgaspar <danielvazgaspar@gmail.com>
2014-04-21 11:15:07 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,13 +17,13 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 """
 Only include this for SQLLite constraints
-
+"""
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
-"""    
+    
 
 from app import views
 
