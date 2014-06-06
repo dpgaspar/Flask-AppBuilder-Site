4619a84ae07b4792551aebf043482c0e88b7f0d6
dpgaspar <danielvazgaspar@gmail.com>
2014-06-06 16:23:06 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -137,8 +137,7 @@ class CountryDirectChartView(DirectByChartView):
         {
             #'label': 'Monthly',
             'group': 'stat_date',
-            'series': ['unemployed',
-                       'college']
+            'series': ['unemployed']
         }
     ]
 
