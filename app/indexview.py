3068dfc6fdecb25be2bcf1daa876bafcefddd72b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-17 18:08:49 +0800

    Delete app directory

--- app/indexview.py
+++
@@ -1,8 +0,0 @@
-from flask_appbuilder.views import IndexView
-
-
-class FABView(IndexView):
-    """
-        A simple view that implements the index for the site
-    """
-    index_template = 'index.html'
