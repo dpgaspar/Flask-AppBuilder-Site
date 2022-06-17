25542b2cfd9b02061648460567fdbb1222a7a757
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-17 18:11:16 +0800

    Delete babel_extract.sh

--- babel_extract.sh
+++
@@ -1,5 +0,0 @@
-pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot app
-pybabel update -N -i ./babel/messages.pot -d app/translations
-pybabel compile -d app/translations
-
-
